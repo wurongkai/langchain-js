d2e21acbd76bdfcebfc8602af322139d0f200ace
wurongkai <945598917@qq.com>
2026-02-21 18:14:14 +0800

    替换open AI为deepseak

--- llm.js
+++ llm.js
@@ -1,4 +1,5 @@
-import { ChatOpenAI } from "@langchain/openai";
+// import { ChatOpenAI } from "@langchain/openai";
+import { ChatDeepSeek } from '@langchain/deepseek'
 import readline from "readline";
 
 // Import environment variables
@@ -13,10 +14,18 @@ const rl = readline.createInterface({
 
 // Create a function to call the Langchain API
 async function chatCompletion(text) {
-  const model = new ChatOpenAI({
-    modelName: "gpt-3.5-turbo",
-    temperature: 0.9,
-  });
+  // const model = new ChatOpenAI({
+  //   modelName: "gpt-3.5-turbo",
+  //   temperature: 0.9,
+  // });
+  // 初始化 DeepSeek 模型（全局单例，避免重复实例化）
+  const model = new ChatDeepSeek({
+    apiKey: process.env.DEEPSEEK_API_KEY,
+    model: 'deepseek-chat',
+    temperature: 0.7, // 控制生成文本的随机性，值越高越有创意
+    maxTokens: 500, // 限制每次响应的最大 token 数量
+    streaming: false // 启用流式输出
+  })
 
   const response = await model.invoke(text);
 
