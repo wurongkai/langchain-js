6d7c67e0cf41a12ff179b9148cabf858bf673536
wurongkai <945598917@qq.com>
2026-02-21 19:58:25 +0800

    格式化输出

--- output-parser.js
+++ output-parser.js
@@ -1,4 +1,4 @@
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatDeepSeek } from '@langchain/deepseek'
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import {
   CommaSeparatedListOutputParser,
@@ -13,10 +13,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 // Instantiate the model
-const model = new ChatOpenAI({
-  modelName: "gpt-3.5-turbo",
-  temperature: 0.9,
-});
+const model = new ChatDeepSeek({
+  apiKey: process.env.DEEPSEEK_API_KEY,
+  model: 'deepseek-chat',
+  temperature: 0.7, // 控制生成文本的随机性，值越高越有创意
+  maxTokens: 500, // 限制每次响应的最大 token 数量
+  streaming: false // 启用流式输出
+})
 
 async function callStringOutputParser() {
   const prompt = ChatPromptTemplate.fromTemplate("Tell a joke about {word}.");
@@ -84,8 +87,8 @@ async function callZodStructuredParser() {
   });
 }
 
-// const response = await callStringOutputParser();
+const response = await callStringOutputParser();
 // const response = await callListOutputParser();
 // const response = await callStructuredParser();
-const response = await callZodStructuredParser();
+// const response = await callZodStructuredParser();
 console.log(response);
