6dd699208a8f8f907d91d31501139141e6194968
wurongkai <945598917@qq.com>
2026-02-21 19:38:04 +0800

    提示词模板

--- prompt-template.js
+++ prompt-template.js
@@ -1,4 +1,5 @@
-import { ChatOpenAI } from "@langchain/openai";
+// import { ChatOpenAI } from "@langchain/openai";
+import { ChatDeepSeek } from '@langchain/deepseek'
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 
 // Import environment variables
@@ -6,10 +7,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 // Instantiate the model
-const model = new ChatOpenAI({
-  modelName: "gpt-3.5-turbo",
-  temperature: 0.9,
-});
+const model = new ChatDeepSeek({
+  apiKey: process.env.DEEPSEEK_API_KEY,
+  model: 'deepseek-chat',
+  temperature: 0.7, // 控制生成文本的随机性，值越高越有创意
+  maxTokens: 500, // 限制每次响应的最大 token 数量
+  streaming: false // 启用流式输出
+})
 
 // Create Prompt Template using fromTemplate
 // const prompt = ChatPromptTemplate.fromTemplate('Tell a joke about {word}');
